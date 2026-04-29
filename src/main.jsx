4f18e02d70970cf01680937e77542bc2f374af94
Fabrizio Nacchia <f.nacchia@esis-italia.com>
2026-04-29 11:40:07 +0200

    tolto scherzo

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Scherzo from './Scherzo.jsx'
 import './index.css'
-
-const hash = window.location.hash.replace('#', '').toLowerCase()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {hash === 'scherzo' ? <Scherzo /> : <App />}
+    <App />
   </React.StrictMode>,
 )
