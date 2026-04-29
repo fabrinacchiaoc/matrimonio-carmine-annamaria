1c4b2cc693fe730f6372ab35c2f331f89129468a
Fabrizio Nacchia <f.nacchia@esis-italia.com>
2026-04-29 11:32:31 +0200

    modifiche

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Scherzo from './Scherzo.jsx'
 import './index.css'
+
+const hash = window.location.hash.replace('#', '').toLowerCase()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {hash === 'scherzo' ? <Scherzo /> : <App />}
   </React.StrictMode>,
 )
